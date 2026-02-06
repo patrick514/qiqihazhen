b7bebe6fa7404673b3020b133f1286af6ff87aa2
patrick514 <2528726609@qq.com>
2026-02-06 13:52:29 +0800

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // 必加：相对路径，解决资源404
+
+  build: {
+    outDir: "docs", // 核心：打包输出到docs文件夹（代替dist）
+  },
   plugins: [
     vue(),
     AutoImport({
